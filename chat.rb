ef6c67e4103b6969bf998e9e15d9d2d6f9e7e3ff
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-26 20:54:11 +0000

    attempt 1

--- chat.rb
+++ chat.rb
@@ -2,12 +2,32 @@
 require "openai"
 require "dotenv/load"
 
-client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
-
-puts "How can I help you today?"
+puts "How can I help you today? (Enter \"bye\" to end the conversation)"
 
 # print separator line
 75.times do 
   print "–"
 end
 print "\n"
+
+client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
+
+user_query = gets.chomp
+
+# prepare an array of messages
+message_list = [
+  {
+    "role" => "user",
+    "content" => user_query
+  }
+]
+
+# Call the API to get the next message from GPT
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+pp api_response
