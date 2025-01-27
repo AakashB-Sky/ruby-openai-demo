b65f552adb5d65f4966dd099b797dedc882b8465
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-27 01:17:49 +0000

    basic loop complete

--- chat.rb
+++ chat.rb
@@ -5,38 +5,42 @@ require "dotenv/load"
 # instantiate a client
 client = OpenAI::Client.new(access_token: ENV.fetch("OPENAI_API_KEY"))
 
-# conversation loop
-
 puts "How can I help you today? (Enter \"bye\" to end the conversation)"
 
-# print separator line
-75.times do 
-  print "–"
+# conversation loop
+end_conversation = false
+
+while not end_conversation # don't forget to add `end`
+  # print separator line
+  75.times do 
+    print "–"
+  end
+  print "\n"
+
+  user_query = gets.chomp
+  
+  # leave loop if user says "bye"
+  if user_query == "bye"
+    puts "Have a nice day!"
+  end_conversation = true
+  else
+    # prepare an array of messages
+    message_list = [
+      {
+        "role" => "user",
+        "content" => user_query
+      }
+    ]
+
+    # Call the API to get the next message from GPT
+    api_response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: message_list
+      }
+    )
+
+    gpt_response = api_response.fetch("choices").[](0).fetch("message").fetch("content")
+    pp gpt_response
+  end
 end
-print "\n"
-
-user_query = gets.chomp
-
-# prepare an array of messages
-message_list = [
-  {
-    "role" => "user",
-    "content" => user_query
-  }
-]
-
-# Call the API to get the next message from GPT
-api_response = client.chat(
-  parameters: {
-    model: "gpt-3.5-turbo",
-    messages: message_list
-  }
-)
-
-=begin
-pp api_response
-pp "<------------- Line Break ---------------->"
-=end 
-
-gpt_response = api_response.fetch("choices").[](0).fetch("message").fetch("content")
-pp gpt_response
